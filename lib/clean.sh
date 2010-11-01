beeb773502aa478cf0d80a492cf572de30f80dcb
Fabio R. Panettieri <fpanettieri@xoomcode.com>
2010-11-01 16:19:26 -0300

    Added clean script

---
+++ lib/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf bin/Makefile \
+  bin/cmake_install.cmake \
+  bin/libams.so \
+  build/CMakeCache.txt \
+  build/CMakeFiles/ \
+  build/Makefile \
+  build/cmake_install.cmake
