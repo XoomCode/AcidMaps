164317554e2380d631eb19810988af3179ded0d2
Fabio R. Panettieri <fpanettieri@xoomcode.com>
2010-11-01 16:24:58 -0300

    Replaced with ignorefile

--- lib/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf bin/Makefile \
-  bin/cmake_install.cmake \
-  bin/libams.so \
-  build/CMakeCache.txt \
-  build/CMakeFiles/ \
-  build/Makefile \
-  build/cmake_install.cmake
