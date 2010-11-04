d959ea55155855b4b0751e5673d95d2b44342471
Carlos Farina <cfarina@gisworking.com>
2010-11-04 15:03:56 -0300

    configuring geoserver project

---
+++ geoserver/plugin/src/main/java/com/xoomcode/acidmaps/core/Bounds.java
@@ -0,0 +1,15 @@
+package com.xoomcode.acidmaps.core;
+
+/**
+ * Bounding Box representation Instances of this class represent bounding
+ * boxes. Data stored as left, bottom, right, top floats. All values are
+ * initialized to 0, however, you should make sure you set them before
+ * using the bounds for anything.
+ */
+public class Bounds {
+
+	public float left;
+	public float bottom;
+	public float right;
+	public float top;
+}
---
+++ geoserver/plugin/src/main/java/com/xoomcode/acidmaps/core/Configuration.java
@@ -0,0 +1,48 @@
+package com.xoomcode.acidmaps.core;
+
+public class Configuration {
+
+	/** Allocated output buffer */
+	/** @note size should be = width * height * color_depth */
+	public byte[] outputBuffer;
+
+	/** Set of valued points, stored 3 floats per point. X, Y, Value */
+	public float[] dataset;
+
+	/** Number of points in the dataset */
+	/** @note Each point is composed of 3 floats, X, Y, Value */
+	public int datasetSize;
+
+	/** Desired simplify method */
+	public int simplifyMethod;
+
+	/** Size of the simplified sample */
+	public int simplifySize;
+
+	/** Requested tile bounds */
+	public Bounds[] tileBounds;
+
+	/** Map bounds */
+	public Bounds[] mapBounds;
+
+	/** Strategy used to interpolate missing values */
+	public int interpolationStrategy;
+
+	/** Interpolated values will be stored here */
+	public int[] interpolatedBitmap;
+
+	/** Output image width */
+	public int width;
+
+	/** Output image height */
+	public int height;
+
+	/** Bits per pixel. Allowed values are 24(RGB) or 32(RGBA) */
+	public int colorDepth;
+
+	/** Interpolation parameter for every interpolation type */
+	public int interpolationParameter;
+
+	public Configuration() {
+	}
+}
