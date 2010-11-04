19c3863d1f94bd9d5918adf2659f07faf6acc3d4
Fabio R. Panettieri <fpanettieri@xoomcode.com>
2010-11-04 15:34:44 -0300

    Fixed paths

--- lib/test/acid_maps_test.cc
+++ lib/test/acid_maps_test.cc
@@ -1,10 +1,14 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 class AcidMapsTest : public CppUnit::TestFixture {
-  CPPUNIT_TEST_SUITE( AcidMapsTest );
-  CPPUNIT_TEST( testEmpty );
+  CPPUNIT_TEST_SUITE(AcidMapsTest);
+  CPPUNIT_TEST( defaultConfiguration );
   CPPUNIT_TEST_SUITE_END();
+  
 public:
-  void testEmpty () { int i = 5; CPPUNIT_ASSERT( i == 5 ); }
+  void defaultConfiguration () {
+    CPPUNIT_ASSERT(true);
+  }
 };
+
 CPPUNIT_TEST_SUITE_REGISTRATION( AcidMapsTest );
