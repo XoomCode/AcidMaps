69b5b3991a909a38e2ede78139e7d27c7a9e6618
Carlos Farina <cfarina@xoomcode.com>
2011-02-22 19:29:37 -0300

    new flex client for demo

---
+++ geoserver/plugin/src/main/java/com/xoomcode/acidmaps/core/Point.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.xoomcode.acidmaps.core;
+
+/**
+ * @date 22/02/2011
+ * @author cfarina
+ *	
+ */
+public class Point {
+	public float x;
+	public float y;
+	public float value;
+}
