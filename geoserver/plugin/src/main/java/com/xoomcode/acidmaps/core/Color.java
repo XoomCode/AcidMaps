28de61328f1b3e1bc108dbe99da5efc3c11af360
Carlos Farina <cfarina@xoomcode.com>
2011-03-14 18:53:24 -0300

    Use color class for colors

---
+++ geoserver/plugin/src/main/java/com/xoomcode/acidmaps/core/Color.java
@@ -0,0 +1,18 @@
+package com.xoomcode.acidmaps.core;
+
+public class Color {
+
+	public byte r;
+	public byte g;
+	public byte b;
+	public byte a;
+	
+	public Color(byte r, byte g, byte b, byte a) {
+		super();
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.a = a;
+	}
+	
+}
---
+++ geoserver/plugin/src/main/java/com/xoomcode/acidmaps/error/AcidMapException.java
@@ -0,0 +1,22 @@
+package com.xoomcode.acidmaps.error;
+
+public class AcidMapException extends Exception {
+
+	public AcidMapException() {
+		super();
+	}
+
+	public AcidMapException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public AcidMapException(String message) {
+		super(message);
+	}
+
+	public AcidMapException(Throwable cause) {
+		super(cause);
+	}
+	
+
+}
